5de6d901da18a2833a78cbf3d0b0452ad7ff8c37
rutuja_mali <rutujamali111@gmail.com>
2022-07-27 02:41:47 +0530

    Employees total daily wage

--- Presenty.java
+++ Presenty.java
@@ -1,18 +1,20 @@
 
 public class Presenty {
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		int IS_FULL_TIME=1;
-		int EMP_RATE_PER_HOUR=20;
-		int empHrs=0;
-		int empWage=0;
-		double empCheck=Math.floor(Math.random()*10)%2;
-		if(empCheck==IS_FULL_TIME)
-			empHrs=8;
-		else
-			empHrs=0;
-		empWage=empHrs*EMP_RATE_PER_HOUR;
-		System.out.println("Daily employee wage: "+ empWage);
-	}
 
+	public static void main(String[] args) {
+		int IS_PART_TIME=1;
+		int IS_FULL_TIME=2;
+		int EMP_RATE_PER_HOUR=20;
+		int EMPhRS=0;
+		int empWage=0;
+		double empCheck=Math.floor(Math.random()*10)%3;
+		if (empCheck==IS_PART_TIME)
+			EMPhRS=4;
+		else if(empCheck==IS_FULL_TIME)
+			EMPhRS=0;
+		else
+			EMPhRS=0;
+		empWage=EMPhRS*EMP_RATE_PER_HOUR;
+		System.out.println("Total Employee Wage: "+empWage);
+	}
 }
