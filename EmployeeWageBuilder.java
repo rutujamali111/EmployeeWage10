b31af7cb274f5061e153ceb2c5d3c0c0f7a0c33b
rutuja_mali <rutujamali111@gmail.com>
2022-07-27 06:16:32 +0530

    Retrieve data by using query

--- EmployeeWageBuilder.java
+++ EmployeeWageBuilder.java
@@ -3,11 +3,15 @@ public class EmployeeWageBuilder {
 
 	public static void main(String[] args) {
 		EmployeeWageComputation employeeWageComputation = new EmployeeWageComputation();
-        employeeWageComputation.addCompany("Microsoft", 4, 30, 100);
+		employeeWageComputation.addCompany("wipro",3,25,140);
+		employeeWageComputation.addCompany("TCS", 4, 30, 100);
+		employeeWageComputation.addCompany("Microsoft", 4, 30, 100);
         employeeWageComputation.addCompany("Google", 5, 40, 170);
         employeeWageComputation.addCompany("Amazon", 19, 10, 150);
         employeeWageComputation.calculateTotalWage();
-
+        String query = "Google";
+        int totalWage = employeeWageComputation.getTotalEmpWage(query);
+        System.out.println("Total Employee Wage for " + query + " company is " + totalWage);
 	}
 
 }
